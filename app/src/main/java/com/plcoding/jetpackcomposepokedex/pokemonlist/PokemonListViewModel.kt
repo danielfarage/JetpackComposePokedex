699502a43400cd24a35f4759ba946dd944250752
Daniel Farage <daniel.farage@yahoo.com.br>
2021-10-02 13:56:52 -0300

    Criado item da lista

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonlist/PokemonListViewModel.kt
@@ -0,0 +1,32 @@
+package com.plcoding.jetpackcomposepokedex.pokemonlist
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
+import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonListViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+    fun calcDominantColor(
+        drawable: Drawable,
+        onFinish: (Color) -> Unit
+    ) {
+        val bitMap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bitMap).generate { pallete ->
+            pallete?.dominantSwatch?.rgb?.let { dominantColor ->
+                onFinish(Color(dominantColor))
+            }
+        }
+
+    }
+
+}
